793ade9fcd2f4b1e3d6ba51c8f4c09f3524f1e16
arazxa <blockchain.ekazy@gmail.com>
2023-12-07 23:22:58 +0500

    contract reset

--- src/App.js
+++ src/App.js
@@ -40,7 +40,9 @@ function App() {
         signer
       );
       const tx = await ct.rate();
-      setrate(parseFloat(parseInt(tx._hex) / 1e18));
+      console.log(tx);
+
+      setrate(parseFloat(parseInt(tx) / 1e18));
       console.log("Transaction receipt:", parseInt(tx._hex));
     } catch (error) {
       // alert(error.code);
@@ -115,13 +117,13 @@ function App() {
                     <h3 className="flex flex-row gap-3 items-center justify-center">
                       {" "}
                       <img width="30" height="30" src={frame} />
-                      <span class="text-darkText text-white  text-sm ">
+                      <span className="text-darkText text-white  text-sm ">
                         1 Token = {rate}MATIC
                       </span>
                     </h3>
                   </div>
 
-                  <div class="w-full  mt-4 text-white rounded-full h-6">
+                  <div className="w-full  mt-4 text-white rounded-full h-6">
                     <p className="p-tag ">La venta está activa</p>
                   </div>
                   {/* <div>
@@ -131,17 +133,17 @@ function App() {
                   </div> */}
 
                   <form>
-                    <div class="flex items-center justify-center py-3 max-w-full mx-auto">
-                      <div class="flex  flex-col  gap-x-3 w-full">
+                    <div className="flex items-center justify-center py-3 max-w-full mx-auto">
+                      <div className="flex  flex-col  gap-x-3 w-full">
                         <div className="flex flex-col ">
                           <label className=" text-white">
                             Cantidad de tokens a comprar{" "}
                           </label>
                         </div>
-                        <div class="flex flex-1 w-full mt-2  rounded justify-between items-center p-0.5 pr-1.5 border-b border-skin-pink dark:border-[#ffffff26]">
+                        <div className="flex flex-1 w-full mt-2  rounded justify-between items-center p-0.5 pr-1.5 border-b border-skin-pink dark:border-[#ffffff26]">
                           <input
                             type="number"
-                            class="bg-transparent flex-1 w-auto text-center z-20 block border-b-[#AB3610]  focus:outline-none text-darkText dark:text-white appearance-none"
+                            className="bg-transparent flex-1 w-auto text-center z-20 block border-b-[#AB3610]  focus:outline-none text-darkText dark:text-white appearance-none"
                             placeholder="0"
                             min={0}
                             onChange={(e) => {
@@ -169,7 +171,7 @@ function App() {
           <div className="w-[90%] max-w-[1440px] p-4 ">
             <div className="grid grid-cols-3 text-white gap-3 mx-auto">
               <div className="text-center rounded-md  px-4 py-8 flex flex-col gap-2">
-                <span class="mas-addons-feature-icon icon-type-icon mb-[30px]">
+                <span className="mas-addons-feature-icon icon-type-icon mb-[30px]">
                   <div className="dot">
                     <img src={one} alt="" />
                   </div>{" "}
@@ -183,7 +185,7 @@ function App() {
                 </p>
               </div>
               <div className="text-center rounded-md  px-4 py-8 flex flex-col gap-2">
-                <span class="mas-addons-feature-icon icon-type-icon mb-[30px]">
+                <span className="mas-addons-feature-icon icon-type-icon mb-[30px]">
                   <div className="dot">
                     <img src={two} alt="" />
                   </div>{" "}
@@ -192,7 +194,7 @@ function App() {
                 <p> Ingresa la cantidad de tokens que deseas comprar.</p>
               </div>
               <div className="text-center rounded-md  px-4 py-8 flex flex-col gap-2">
-                <span class="mas-addons-feature-icon icon-type-icon mb-[30px]">
+                <span className="mas-addons-feature-icon icon-type-icon mb-[30px]">
                   <div className="dot">
                     <img src={three} alt="" />
                   </div>
@@ -208,8 +210,8 @@ function App() {
           <div className="w-[90%] max-w-[1440px] p-4 ">
             <div className="grid grid-cols-2 text-white gap-4 mx-auto">
               <div>
-                <div class="ytp-cued-thumbnail-overlay">
-                  <div class="ytp-cued-thumbnail-overlay-image">
+                <div className="ytp-cued-thumbnail-overlay">
+                  <div className="ytp-cued-thumbnail-overlay-image">
                     <iframe
                       width="524"
                       height="295"
@@ -223,11 +225,11 @@ function App() {
                 </div>
               </div>
 
-              <div class=" px-[30px]">
+              <div className=" px-[30px]">
                 <div className="">
-                  <h2 class="elementor-element">Cómo funciona?​</h2>
+                  <h2 className="elementor-element">Cómo funciona?​</h2>
                 </div>
-                <p class="  mt-5 text-white ">
+                <p className="  mt-5 text-white ">
                   Al integrar perfectamente la tecnología blockchain, Emach
                   pretende revolucionar la forma en que se realizan las
                   transacciones y los servicios dentro de este dominio. Como
@@ -247,12 +249,12 @@ function App() {
           </div>
         </div>
 
-        <div class="bg-[#F2F2F2] flex justify-between">
+        <div className="bg-[#F2F2F2] flex justify-between">
           <img className="mt-40" src={sidel} alt="" />
           <div className="flex -mt-52 text-black text-center items-center flex-col justify-center">
-            <h1 class="text-4xl  font-semibold">Token Emach</h1>
+            <h1 className="text-4xl  font-semibold">Token Emach</h1>
             <img className="mb-8" src={token} alt="" />
-            <p class="font-black text-xl my-8">Flujo Emach</p>
+            <p className="font-black text-xl my-8">Flujo Emach</p>
             <p>
               {" "}
               La fortaleza sistemática del Token Emach será abastecida por el
@@ -263,15 +265,19 @@ function App() {
           <img className="mt-40" src={sider} alt="" />
         </div>
 
-        <footer class="bg-black pt-34">
-          <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <footer className="bg-black pt-34">
+          <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
 
-          <div class="mx-auto w-[90%] max-w-[1440px] p-4 py-6 lg:py-8">
-            <div class="md:flex md:justify-between">
-              <div class="mb-6 md:mb-0">
-                <a href="#" class="flex items-center">
-                  <img src={emachlogo} class="h-8 mr-3" alt="FlowBite Logo" />
-                  {/* <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+          <div className="mx-auto w-[90%] max-w-[1440px] p-4 py-6 lg:py-8">
+            <div className="md:flex md:justify-between">
+              <div className="mb-6 md:mb-0">
+                <a href="#" className="flex items-center">
+                  <img
+                    src={emachlogo}
+                    className="h-8 mr-3"
+                    alt="FlowBite Logo"
+                  />
+                  {/* <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
                     Flowbite
                   </span> */}
                 </a>
@@ -280,51 +286,51 @@ function App() {
                   nosotros por medio <br></br>del chat para solventar tus dudas.{" "}
                 </p>
               </div>
-              <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
+              <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
                 <div>
-                  <h2 class="mb-6 text-sm font-semibold text-white uppercase dark:text-white">
+                  <h2 className="mb-6 text-sm font-semibold text-white uppercase dark:text-white">
                     Productos
                   </h2>
-                  <ul class="text-gray-500 dark:text-gray-400 font-medium">
-                    <li class="">
-                      <a class="hover:underline">Crypto</a>
+                  <ul className="text-gray-500 dark:text-gray-400 font-medium">
+                    <li className="">
+                      <a className="hover:underline">Crypto</a>
                     </li>
                     <li>
-                      <a class="hover:underline">NFT</a>
+                      <a className="hover:underline">NFT</a>
                     </li>
                     <li>
-                      <a class="hover:underline">Juegos</a>
+                      <a className="hover:underline">Juegos</a>
                     </li>
                   </ul>
                 </div>
                 <div>
-                  <h2 class="mb-6 text-sm font-semibold text-white uppercase dark:text-white">
+                  <h2 className="mb-6 text-sm font-semibold text-white uppercase dark:text-white">
                     Empresa
                   </h2>
-                  <ul class="text-gray-500 dark:text-gray-400 font-medium">
-                    <li class="">
-                      <a class="hover:underline ">Inicio </a>
+                  <ul className="text-gray-500 dark:text-gray-400 font-medium">
+                    <li className="">
+                      <a className="hover:underline ">Inicio </a>
                     </li>
                     <li>
-                      <a class="hover:underline">Acerca de </a>
+                      <a className="hover:underline">Acerca de </a>
                     </li>
                     <li>
-                      <a class="hover:underline">Tutorial </a>
+                      <a className="hover:underline">Tutorial </a>
                     </li>
                     <li>
-                      <a class="hover:underline">Legal </a>
+                      <a className="hover:underline">Legal </a>
                     </li>
                   </ul>
                 </div>
                 <div>
-                  <h2 class="mb-6 text-sm font-semibold text-white uppercase dark:text-white">
+                  <h2 className="mb-6 text-sm font-semibold text-white uppercase dark:text-white">
                     Contacto
                   </h2>
-                  <ul class="text-gray-500 dark:text-gray-400 font-medium">
+                  <ul className="text-gray-500 dark:text-gray-400 font-medium">
                     <li>
                       <a
                         href="https://discord.gg/4eeurUVvTy"
-                        class="hover:underline"
+                        className="hover:underline"
                       >
                         token@emach.mx
                       </a>
@@ -333,21 +339,21 @@ function App() {
                 </div>
               </div>
             </div>
-            <div class="sm:flex sm:items-center sm:justify-between">
-              <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+            <div className="sm:flex sm:items-center sm:justify-between">
+              <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
                 © 2023{" "}
-                <a href="#" class="hover:underline">
+                <a href="#" className="hover:underline">
                   Emach
                 </a>
                 . All Rights Reserved.
               </span>
-              <div class="flex mt-4 space-x-5 sm:justify-center sm:mt-0">
+              <div className="flex mt-4 space-x-5 sm:justify-center sm:mt-0">
                 <a
                   href="#"
-                  class="text-gray-500 hover:text-gray-900 dark:hover:text-white"
+                  className="text-gray-500 hover:text-gray-900 dark:hover:text-white"
                 >
                   <svg
-                    class="w-4 h-4"
+                    className="w-4 h-4"
                     aria-hidden="true"
                     xmlns="http://www.w3.org/2000/svg"
                     fill="currentColor"
@@ -359,14 +365,14 @@ function App() {
                       clip-rule="evenodd"
                     />
                   </svg>
-                  <span class="sr-only">Facebook page</span>
+                  <span className="sr-only">Facebook page</span>
                 </a>
                 <a
                   href="#"
-                  class="text-gray-500 hover:text-gray-900 dark:hover:text-white"
+                  className="text-gray-500 hover:text-gray-900 dark:hover:text-white"
                 >
                   <svg
-                    class="w-4 h-4"
+                    className="w-4 h-4"
                     aria-hidden="true"
                     xmlns="http://www.w3.org/2000/svg"
                     fill="currentColor"
@@ -374,14 +380,14 @@ function App() {
                   >
                     <path d="M16.942 1.556a16.3 16.3 0 0 0-4.126-1.3 12.04 12.04 0 0 0-.529 1.1 15.175 15.175 0 0 0-4.573 0 11.585 11.585 0 0 0-.535-1.1 16.274 16.274 0 0 0-4.129 1.3A17.392 17.392 0 0 0 .182 13.218a15.785 15.785 0 0 0 4.963 2.521c.41-.564.773-1.16 1.084-1.785a10.63 10.63 0 0 1-1.706-.83c.143-.106.283-.217.418-.33a11.664 11.664 0 0 0 10.118 0c.137.113.277.224.418.33-.544.328-1.116.606-1.71.832a12.52 12.52 0 0 0 1.084 1.785 16.46 16.46 0 0 0 5.064-2.595 17.286 17.286 0 0 0-2.973-11.59ZM6.678 10.813a1.941 1.941 0 0 1-1.8-2.045 1.93 1.93 0 0 1 1.8-2.047 1.919 1.919 0 0 1 1.8 2.047 1.93 1.93 0 0 1-1.8 2.045Zm6.644 0a1.94 1.94 0 0 1-1.8-2.045 1.93 1.93 0 0 1 1.8-2.047 1.918 1.918 0 0 1 1.8 2.047 1.93 1.93 0 0 1-1.8 2.045Z" />
                   </svg>
-                  <span class="sr-only">Discord community</span>
+                  <span className="sr-only">Discord community</span>
                 </a>
                 <a
                   href="#"
-                  class="text-gray-500 hover:text-gray-900 dark:hover:text-white"
+                  className="text-gray-500 hover:text-gray-900 dark:hover:text-white"
                 >
                   <svg
-                    class="w-4 h-4"
+                    className="w-4 h-4"
                     aria-hidden="true"
                     xmlns="http://www.w3.org/2000/svg"
                     fill="currentColor"
@@ -393,7 +399,7 @@ function App() {
                       clip-rule="evenodd"
                     />
                   </svg>
-                  <span class="sr-only">Twitter page</span>
+                  <span className="sr-only">Twitter page</span>
                 </a>
               </div>
             </div>
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -3,21 +3,21 @@ import logo from "./logosin.png";
 export default function Navbar() {
   return (
     <>
-      <nav class="bg-[#201D1D] border-gray-200 ">
-        <div class="max-w-screen-xl flex flex-wrap items-center justify-around mx-auto p-4">
-          <a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
-            <img src={logo} class="h-8" alt="Logo" />
+      <nav className="bg-[#201D1D] border-gray-200 ">
+        <div className=" flex flex-wrap items-center justify-between mx-auto p-4  w-[90%] max-w-[1440px]">
+          <a href="/" className="flex space-x-3 rtl:space-x-reverse">
+            <img src={logo} className="h-8" alt="Logo" />
           </a>
           <button
             data-collapse-toggle="navbar-default"
             type="button"
-            class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 "
+            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 "
             aria-controls="navbar-default"
             aria-expanded="false"
           >
-            <span class="sr-only">Open main menu</span>
+            <span className="sr-only">Open main menu</span>
             <svg
-              class="w-5 h-5"
+              className="w-5 h-5"
               aria-hidden="true"
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -32,12 +32,12 @@ export default function Navbar() {
               />
             </svg>
           </button>
-          <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-            <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 ">
+          <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+            <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 ">
               <li>
                 <a
                   href="#"
-                  class="block py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 "
+                  className="block py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 "
                 >
                   Inicio
                 </a>
@@ -45,7 +45,7 @@ export default function Navbar() {
               <li>
                 <a
                   href="#"
-                  class="block py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 "
+                  className="block py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 "
                 >
                   Documentos
                 </a>
